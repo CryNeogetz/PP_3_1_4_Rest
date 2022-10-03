c3145cf53cfb2d8fea2a9f8c39308b9dff9a870f
cryneogetz <C:\Users\Neogetz\AppData\Roaming\The Bat!>
2022-10-03 15:42:06 +0300

    removed garbage

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -11,14 +11,10 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -44,21 +40,8 @@ public class AdminController {
         return "admin/admin";
     }
 
-//    @GetMapping("/{id}")
-//    public String showUser(@PathVariable("id") int id, Model model) {
-//        model.addAttribute("user", userService.findUserById(id));
-//        model.addAttribute("roleList", roleService.findAll());
-//        return "/admin/edit";
-//    }
-
     @PatchMapping("/{id}")
     public String updateUser(@ModelAttribute("user") User user) {
-
-//        System.out.println(roles + roles);
-//
-//        List<Role> roleList = new ArrayList<>();
-//        roleList.add(roleService.findAll().get(roles-1));
-//        user.setRoles(roleList);
 
         userService.updateUserById(user);
         return "redirect:/admin";
@@ -76,13 +59,4 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-//    @PostMapping("/admin")
-//    public String  deleteUser(@RequestParam(required = true, defaultValue = "" ) int userId,
-//                              @RequestParam(required = true, defaultValue = "" ) String action,
-//                              Model model) {
-//        if (action.equals("delete")){
-//            userService.deleteUser(userId);
-//        }
-//        return "redirect:/admin";
-//    }
 }
